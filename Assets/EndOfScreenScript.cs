87309e160a8aed50c9ddcfb8d99cdd74d0f1b129
Damian Brdej <brdej98@gmail.com>
2023-10-26 21:58:21 +0200

    stop game when game over

--- Assets/EndOfScreenScript.cs
+++ Assets/EndOfScreenScript.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class EndOfScreenScript : MonoBehaviour
 {
     public LogicScript logic;
+    public BirdScript bird;
     private void OnTriggerEnter2D(Collider2D collison)
     {
         if (collison.gameObject.layer == 3)
         {
+            bird.birdIsAlive = false;
             logic.gameOver();
         }
     }
--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -6,21 +6,37 @@ public class PipeMoveScript : MonoBehaviour
 {
     public float moveSpeed = 5;
     public float deadZone = -45;
+    public GameObject gameOverScreen;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOverScreen = FindInactiveObjectWithTag("gameOver");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if (!gameOverScreen.activeInHierarchy)
+        {
+            transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        }
 
         if (transform.position.x < deadZone)
         {
             Destroy(gameObject);
         }
+    }
+    
+    GameObject FindInactiveObjectWithTag(string tag)
+    {
+        foreach (GameObject obj in Resources.FindObjectsOfTypeAll<GameObject>()) 
+        {
+            if (obj.tag == tag && !obj.scene.name.Equals(null))
+            {
+                return obj;
+            }
+        }
+        return null;
     }
 }
